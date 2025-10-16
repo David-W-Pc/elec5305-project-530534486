7b0047b4797b353662ea697eb8bfa6f29a44a9d1
David-W-Pc <wpcdavid@outlook.com>
2025-10-16 22:25:28 +1100

    Delete src directory

--- src/loadEsc50Meta.m
+++
@@ -1,6 +0,0 @@
-function T = loadEsc50Meta(metaCsvPath)
-    opts = detectImportOptions(metaCsvPath);
-    T = readtable(metaCsvPath, opts);
-    T = T(:, {'filename','fold','target','category'});
-    T.category = categorical(string(T.category));
-end
